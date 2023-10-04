a08e300ca5b07faacc4aa0ba477df9e2b9c93036
IRedDragonICY <hendik.suwoto@gmail.com>
2023-10-04 19:41:38 +0700

    first build

---
+++ src/main/java/com/example/compactjv/HelloApplication.java
@@ -0,0 +1,26 @@
+package com.example.compactjv;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 500, 780);
+        stage.initStyle(StageStyle.UNDECORATED); // Menambahkan baris ini
+        stage.setResizable(false);
+        stage.setTitle("CompactJV");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
